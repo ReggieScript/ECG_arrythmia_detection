ce73fffabc88b82cfe0bd8da88a2690731f2f857
Regina <regina.crespo.018@gmail.com>
2023-05-26 12:19:49 -0600

    Added some style

--- UserInterface.py
+++ UserInterface.py
@@ -12,14 +12,14 @@ import main
 # Create the main window
 window = tk.Tk()
 window.title("ECG Arrhythmia Detector V 1.0")
-window.configure(bg="#87CEEB")  # Set background color to sky blue
+window.configure(bg="#87CEEB")  # Set background color to sky blue #87CEEB
 
 # Add a label for the main title
-title_label = tk.Label(window, text="ECG Arrythmia Detector V 1.0", font=("Roboto", 24, "bold"), fg="#000000", bg="#87CEEB", anchor="w")
+title_label = tk.Label(window, text="ECG Arrythmia Detector V 1.0", font=("Roboto", 24, "bold"), fg="#212431", bg="#87CEEB", anchor="w")
 title_label.pack(pady=20, padx=(10, 0), anchor="w")  # Add left margin
 
 # Add a label for the subtitle
-subtitle_label = tk.Label(window, text="Designed by Regina Crespo, Miguel Gaona, Ivan Garza and Lizeth Ramirez", font=("Roboto", 12, "italic"), fg="#000000", bg="#87CEEB", anchor="w")
+subtitle_label = tk.Label(window, text="Designed by Regina Crespo, Miguel Gaona, Ivan Garza and Lizeth Ramirez", font=("Roboto", 12, "italic"), fg="#212431", bg="#87CEEB", anchor="w")
 subtitle_label.pack(pady=(0, 20), padx=(10, 0), anchor="w")  # Add left margin
 
 # Variable to store the selected file path
@@ -47,18 +47,19 @@ file_frame = tk.Frame(window, bg="#87CEEB")
 file_frame.pack(pady=10)
 
 # Add a button to select a file
-browse_button = tk.Button(file_frame, text="Select ECG File", command=browse_file, font=("Arial", 12), fg="#000000", bg="#FFFFFF")
+file_btn = PhotoImage(file = r"Visuals\button_select-ecg-file.png")
+browse_button = tk.Button(file_frame, image= file_btn, command=browse_file, font=("Arial", 12), bg="#87CEEB", highlightthickness=0, bd =0, border=0, borderwidth=0)
 browse_button.pack(padx=(10, 0))  # Add left margin
 
 # Add a label to display the selected file
 derivation_list = [0]
-file_label = tk.Label(window, text="No file selected", font=("Roboto", 12), fg="#000000", bg="#87CEEB", anchor="center")
+file_label = tk.Label(window, text="No file selected", font=("Roboto", 12), fg="#212431", bg="#87CEEB", anchor="center")
 file_label.pack(pady=(5, 10), padx=(10, 0))  # Add left margin and center alignment
 
 selected_number = tk.StringVar(window)
 selected_number.set(derivation_list[0])  # Set the default selected derivation
 
-derivation_label = tk.Label(window, text="Derivation:", font=("Arial", 12), fg="#000000", bg="#87CEEB")
+derivation_label = tk.Label(window, text="Derivation:", font=("Arial", 12), fg="#212431", bg="#87CEEB")
 derivation_label.pack(anchor="w", padx=(10, 0))  # Add left margin
 
 derivation_combobox = ttk.Combobox(window, values=derivation_list, textvariable=selected_number, font=("Arial", 12), postcommand=update_list)
@@ -67,7 +68,7 @@ derivation_combobox.pack(anchor="w", padx=(10, 0))  # Add left margin
 # Add a drop-down list (combobox) to select a derivation
 
 # Add a field to insert the frequency
-frequency_label = tk.Label(window, text="Frequency:", font=("Arial", 12), fg="#000000", bg="#87CEEB")
+frequency_label = tk.Label(window, text="Frequency:", font=("Arial", 12), fg="#212431", bg="#87CEEB")
 frequency_label.pack(anchor="w", padx=(10, 0))  # Add left margin
 
 frequency_entry = tk.Entry(window, font=("Arial", 12))
@@ -77,10 +78,19 @@ frequency_entry.pack(anchor="w", padx=(10, 0))  # Add left margin
 blank_space_label = tk.Label(window, text="", font=("Arial", 6), bg="#87CEEB")
 blank_space_label.pack()
 
+# Create a progressbar
+
+progressbar = ttk.Progressbar(mode = "indeterminate")
+
 # Function to handle button click event for initiating the ECG analysis process
 def initiate_process():
     if str(selected_number.get()) != '0':
         if selected_file_path:
+
+            progressbar.pack(anchor = "e", padx = (10,10))
+
+            progressbar.start()
+
             # Add your code
             full_record, full_record_data, n_samples, frequency, channels = main.first_step(selected_file_path)
             selected_frequency = frequency_entry.get()
@@ -106,38 +116,44 @@ def initiate_process():
             
             plt.show()
             
+            
+
             ecg_data, ecg_df, bad_quality, good_quality, final_df, result = main.third_step(selected_file_path, n_samples, frequency, selected_derivation)
-            abnormalities_text.configure(text=f"Abnormalities found: {len(result)}")
+            abnormalities_text.configure(text=f"Abnormalities found: {result.count(1)}")
             bad_quality_text.configure(text=f"Bad quality segments: {len(bad_quality)}")
 
+            progressbar.stop()
+
+            # messagebox.showinfo(title = "RESULTS", message = f"Abnormalities found: {result.count(1)} \n Bad quality segments: {len(bad_quality)}")
             # Display a message box with the analysis results
         else:
             messagebox.showwarning("File Not Selected", "Please select an ECG file.")
     else:
         messagebox.showwarning("Derivation not selected", "Please select a derivation.")
 # Add a button to initiate the ECG analysis process
-initiate_button = tk.Button(window, text="Run!", command=initiate_process, font=("Arial", 14, "bold"), fg="#000000", bg="#FFFFFF")
+run_btn = PhotoImage(file = r"Visuals\button_run.png")
+initiate_button = tk.Button(window, image = run_btn, command=initiate_process, highlightthickness=0, bd = 0, border= 0, bg = "#87CEEB")
 initiate_button.pack(anchor="w", padx=(10, 0), pady=(20, 0))  # Add left margin
 
 # Add a section to display the result
-result_label = tk.Label(window, text="Result:", font=("Arial", 16, "bold"), fg="#000000", bg="#87CEEB")
+result_label = tk.Label(window, text="Result:", font=("Arial", 16, "bold"), fg="#212431", bg="#87CEEB")
 result_label.pack(side=tk.RIGHT)
 
 # Add a label for the abnormalities count
-abnormalities_text = tk.Label(window, text="", font=("Roboto", 14), fg="#000000", bg="#87CEEB")
+abnormalities_text = tk.Label(window, text="", font=("Roboto", 14), fg="#212431", bg="#87CEEB")
 abnormalities_text.pack(side=tk.RIGHT, padx=10)
 
 # Add a label for the bad quality moments count
-bad_quality_text = tk.Label(window, text="", font=("Roboto", 14), fg="#000000", bg="#87CEEB")
+bad_quality_text = tk.Label(window, text="", font=("Roboto", 14), fg="#212431", bg="#87CEEB")
 bad_quality_text.pack(side=tk.RIGHT, padx=10)
 
 # Add a label for the heat map result
-heat_map_label = tk.Label(window, text="Heat Map:", font=("Roboto", 12), fg="#000000", bg="#87CEEB")
-heat_map_label.pack(anchor="w", padx=(10, 0))  # Add left margin
+# heat_map_label = tk.Label(window, text="Heat Map:", font=("Roboto", 12), fg="#212431", bg="#87CEEB")
+# heat_map_label.pack(anchor="w", padx=(10, 0))  # Add left margin
 
 # Add a label to display the heat map result
-heat_map_text = tk.Label(window, text="", font=("Roboto", 12), fg="#000000", bg="#87CEEB")
-heat_map_text.pack(anchor="w", padx=(10, 0))  # Add left margin
+# heat_map_text = tk.Label(window, text="", font=("Roboto", 12), fg="#212431", bg="#87CEEB")
+# heat_map_text.pack(anchor="w", padx=(10, 0))  # Add left margin
 
 # Start the main loop
 window.mainloop()
